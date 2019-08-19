52486cb731307ae253b1b6a95471344b12bb64d2
Wayne <cww570063013@gmail.com>
2019-08-19 15:08:15 +0800

    add a simple queue task example code

--- index.py
+++ index.py
@@ -9,21 +9,42 @@ from website.models import db
 from admin.models import db as db2
 from website.repositories.home import index
 from flask_admin import Admin
+from flask_celery import make_celery
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
 app.config['SQLALCHEMY_BINDS'] = SQLALCHEMY_BINDS
+app.config.update(
+    CELERY_BROKER_URL='redis://localhost:6379',
+    CELERY_RESULT_BACKEND='redis://localhost:6379'
+)
 app.secret_key = SECRET_KEY
 db.init_app(app)
 db2.init_app(app)
+
+celery = make_celery(app)
+
+
+@app.route('/test_celery')
+def test_celery():
+    add_together.delay('sss', 'bbb')
+    return 'shit'
+
+
+@celery.task()
+def add_together(a, b):
+    file = 'aaa.log'
+    with open(file, 'a') as fo:
+        fo.write(a + b + "\n")
+    return a + b
+
 
 # set optional bootswatch theme
 app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 
 admin = Admin(app, name='microblog', template_mode='bootstrap3')
-
 
 app.register_blueprint(payment.payment_bp)
 app.register_blueprint(manual_admin.admin)
