ec16468800d17829c73be6a3b6080443e9265d9f
wayne <570063013@qq.com>
2020-04-24 14:17:34 +0800

    tg bot debug

--- tg_bot/routes.py
+++ tg_bot/routes.py
@@ -10,8 +10,13 @@ tg = Blueprint('tg', __name__, url_prefix='/tg')
 
 bot_key = '1051400474:AAEkNqkdvOem7_mLJwpnjSHEcrsYSbuE8Fk'
 top_10btc_api = 'https://nomics.com/data/currencies/ticker?include-transparency=false&interval=1d,30d&labels=0&limit=10&quote-currency=USD&start=0'
+cryptoprice = '/cryptoprice'
+cPrice_notice_higher_than = '/cPrice_notice_higher_than'
+cPrice_notice_lower_than = '/cPrice_notice_lower_than'
 valid_commands = [
-    '/cryptoprice'
+    '/cryptoprice',
+    '/cPrice_notice_higher_than',
+    '/cPrice_notice_lower_than'
 ]
 
 
@@ -29,7 +34,13 @@ def setup():
 def parse_msg(msg):
     chat_id = msg['message']['chat']['id']
     tex = msg['message']['text']
-    return chat_id, tex
+    command = tex
+    param = ''
+    space_p = tex.find(' ')
+    if space_p > 0:
+        command = tex[:space_p]
+        param = tex[tex.rfind(' ') + 1:]
+    return chat_id, tex, command, param
 
 
 def send_msg(chat_id, text='bla-bla-bal--'):
@@ -53,22 +64,27 @@ def get_top10_crypto():
     return price_date, final_list
 
 
+def crypto_currency_price(chat_id):
+    price_date, final_list = get_top10_crypto()
+    text = '*' + price_date + '*' + "\n" + '*crypto   price   1day_change_pct   30d_change_pct*' + "\n"
+    for i in final_list:
+        price = str(round(float(i['price']), 4))
+        d_change_pct = str(round(float(i['1d_price_change_pct']) * 100, 1)) + '%'
+        d30_change_pct = str(round(float(i['30d_price_change_pct']) * 100, 1)) + '%'
+        text += i['name'] + '   *' + price + '*   ' + d_change_pct + '   ' + d30_change_pct + "\n"
+    send_msg(chat_id, text)
+
+
 @tg.route('/web_hook', methods=['POST', 'GET'])
 def web_hook():
     if request.method == 'POST':
         msg = request.get_json()
-        chat_id, tex = parse_msg(msg)
-        if tex not in valid_commands:
+        chat_id, tex, command, param = parse_msg(msg)
+        if command not in valid_commands:
             send_msg(chat_id, 'command doesn\'t exist')
             return Response('ok', status=200)
-        price_date, final_list = get_top10_crypto()
-        text = '*' + price_date + '*' + "\n" + '*crypto   price   1day_change_pct   30d_change_pct*' + "\n"
-        for i in final_list:
-            price = str(round(float(i['price']), 4))
-            d_change_pct = str(round(float(i['1d_price_change_pct']) * 100, 1)) + '%'
-            d30_change_pct = str(round(float(i['30d_price_change_pct']) * 100, 1)) + '%'
-            text += i['name'] + '   *' + price + '*   ' + d_change_pct + '   ' + d30_change_pct + "\n"
-        send_msg(chat_id, text)
+        if command == cryptoprice:
+            crypto_currency_price(chat_id)
 
         return Response('ok', status=200)
     else:
